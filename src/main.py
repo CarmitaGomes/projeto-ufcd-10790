35b499ee048ecfae6a86d16df686bc9ebebf56fe
Carmita <125024649+CarmitaGomes@users.noreply.github.com>
2026-06-12 09:59:22 +0100

    criada função inicializar perfil (main)

--- src/main.py
+++ src/main.py
@@ -1,11 +1,35 @@
 # Aqui vai o código principal do seu projeto
 #
+from src.ler_gravar import carregar_dados, guardar_dados
 
+def inicializar_perfil(dados):
+    if dados["perfil"]["nome"] == "":
+        print("\n=========================================")
+        print("    BEM-VINDA AO CONFIGURADOR DE PERFIL  ")
+        print("=========================================")
+
+        nome = input("Introduza o seu nome: ").strip()
+        idade = int(input("Introduza a sua idade: "))
+        print("Introduzir data no formato AAAA-MM-DD")
+        ultima_mes = input("Data da ultima menstruação: ").strip()
+
+        dados["perfil"]["nome"] == nome
+        dados["perfil"]["idade"] == idade
+        dados["perfil"]["ultima_menstruacao"] == ultima_mes
+
+        guardar_dados(dados)
+        print("\n[Boa] Perfil gravado no ciclo_dados.json!")
+    else:
+        print(f"\nOlá outra vez, {dados['perfil']['nome']}!")
+    
+    return dados
 
 
 
 def main():
-    print("Olá, mundo! Este é o ponto de entrada do meu projeto.")
+    print("=== PERIOD TRACKER ===")
+    dados = carregar_dados()
+    dados = inicializar_perfil()
     # Você pode adicionar mais funcionalidades aqui
 
 
